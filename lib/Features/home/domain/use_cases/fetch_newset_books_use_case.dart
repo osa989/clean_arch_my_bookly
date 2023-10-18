e1e01cfa461d21d673346445df39d0f594c84c2b
osa989 <osamagamal>
2023-10-18 11:53:23 +0300

    fetch newset books use case

---
+++ lib/Features/home/domain/use_cases/fetch_newset_books_use_case.dart
@@ -0,0 +1,15 @@
+import 'package:dartz/dartz.dart';
+import 'package:my_bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:my_bookly/Features/home/domain/repos/home_repo.dart';
+import 'package:my_bookly/core/errors/failure.dart';
+import 'package:my_bookly/core/use_cases/use_case.dart';
+
+class FetchNewsetBooksUseCase extends UseCase<List<BookEntity>, NoParam> {
+  final HomeRepo homeRepo;
+  FetchNewsetBooksUseCase(this.homeRepo);
+  
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([NoParam? param])async {
+  return await homeRepo.fetchNewsetBooks();
+  }
+}
