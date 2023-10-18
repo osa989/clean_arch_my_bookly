ad61c1571a7eb67c4ed44517cc400d5128581e93
osa989 <osamagamal>
2023-10-18 10:58:09 +0300

    add dartz package and add either class

--- lib/Features/home/domain/repos/home_repo.dart
+++ lib/Features/home/domain/repos/home_repo.dart
@@ -1,6 +1,7 @@
 import 'package:my_bookly/Features/home/domain/entities/book_entity.dart';
-
+import 'package:my_bookly/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
 abstract class HomeRepo {
-  Future<List<BookEntity>> fetchFeaturedBooks();
-  Future<List<BookEntity>> fetchNewsetBooks();
+  Future<Either<Failure,List<BookEntity>>> fetchFeaturedBooks();
+  Future<Either<Failure,List<BookEntity>>> fetchNewsetBooks();
 }
