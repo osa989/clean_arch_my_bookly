4a5b21a8ee0bf9b71f302c2245401728fc69b985
osa989 <osamagamal>
2023-10-21 09:37:52 +0300

    implemet remote fetch newest books

--- lib/Features/home/data/data_sources/home_remote_data_source.dart
+++ lib/Features/home/data/data_sources/home_remote_data_source.dart
@@ -21,8 +21,6 @@ class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
 
   @override
   Future<List<BookEntity>> fetchNewsetBooks() async{
-
-
     var data = await apiService.get(
         endPoint: 'volumes?Filtering=free-ebooks&Sorting=newest&q=programming');
     List<BookEntity> books = getBooksList(data);
