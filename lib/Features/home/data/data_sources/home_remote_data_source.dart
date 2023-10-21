d681a285bba75134f2125b6d7a5d246efc6543b9
osa989 <osamagamal>
2023-10-21 13:54:03 +0300

    cache featured box

--- lib/Features/home/data/data_sources/home_remote_data_source.dart
+++ lib/Features/home/data/data_sources/home_remote_data_source.dart
@@ -1,6 +1,9 @@
+import 'package:hive_flutter/adapters.dart';
 import 'package:my_bookly/Features/home/data/models/book_model/book_model.dart';
 import 'package:my_bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:my_bookly/constants.dart';
 import 'package:my_bookly/core/utils/api_service.dart';
+import 'package:my_bookly/core/utils/functions/save_books.dart';
 
 abstract class HomeRemoteDataSource {
   Future<List<BookEntity>> fetchFeaturedBooks();
@@ -15,24 +18,23 @@ class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
     var data = await apiService.get(
         endPoint: 'volumes?Filtering=free-ebooks&q=programming');
     List<BookEntity> books = getBooksList(data);
+
+    saveBooksData(books,kFeaturedBox);
     return books;
   }
 
 
+
   @override
-  Future<List<BookEntity>> fetchNewsetBooks() async{
+  Future<List<BookEntity>> fetchNewsetBooks() async {
     var data = await apiService.get(
         endPoint: 'volumes?Filtering=free-ebooks&Sorting=newest&q=programming');
     List<BookEntity> books = getBooksList(data);
     return books;
   }
-  
-
-
-
 
   List<BookEntity> getBooksList(Map<String, dynamic> data) {
-       List<BookEntity> books = [];
+    List<BookEntity> books = [];
     for (var bookMap in data['items']) {
       books.add(BookModel.fromJson(bookMap));
     }
