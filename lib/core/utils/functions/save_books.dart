3ee2006309a22bbe711752665211728ea04e30fd
osa989 <osamagamal>
2023-10-25 18:32:36 +0300

    Solve Hive Error

--- lib/core/utils/functions/save_books.dart
+++ lib/core/utils/functions/save_books.dart
@@ -2,6 +2,6 @@
 import 'package:my_bookly/Features/home/domain/entities/book_entity.dart';
 
 void saveBooksData(List<BookEntity> books,String boxName) {
-       var box = Hive.box(boxName);
+       var box = Hive.box<BookEntity>(boxName);
     box.addAll(books);
   }
